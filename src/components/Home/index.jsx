3c3d5595106daf6f2bac5b274dc8634902bbed67
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-04-09 23:45:51 +0600

    static job category list added in home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Header from "./Header";
+import CategoryCard from "../CategoryCard";
 
 const Home = () => {
   return (
@@ -8,6 +9,21 @@ const Home = () => {
       <Navbar />
       <div className="mt-8">
         <Header />
+      </div>
+      <div className="max-w-screen-xl mx-auto my-24 px-4">
+        <div className="text-center mb-10 space-y-5">
+          <h2 className="font-bold text-5xl">Job Category List</h2>
+          <p className="text-gray-400">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+          <CategoryCard></CategoryCard>
+          <CategoryCard></CategoryCard>
+          <CategoryCard></CategoryCard>
+          <CategoryCard></CategoryCard>
+        </div>
       </div>
     </div>
   );
